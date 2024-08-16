2db147c2872ea8cd97f7c09ded23e752501e1e9d
ry-jp <marketing@vincor.com>
2024-08-16 11:21:32 -0500

    image blur return

--- woonuxt_base/app/components/productElements/ProductCard.vue
+++ woonuxt_base/app/components/productElements/ProductCard.vue
@@ -47,7 +47,9 @@ const imagetoDisplay = computed<string>(() => {
         :src="imagetoDisplay"
         :alt="node.image?.altText || node.name || 'Product image'"
         :title="node.image?.title || node.name"
-
+        :loading="index <= 3 ? 'eager' : 'lazy'"
+        placeholder
+        placeholder-class="blur-xl"
 class="skeleton"
 
          />
