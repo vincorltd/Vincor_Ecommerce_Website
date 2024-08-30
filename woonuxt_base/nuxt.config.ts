b3442bf82c9e05563c2c32448c68eb34fab078fd
ry-jp <marketing@vincor.com>
2024-08-30 11:51:33 -0500

    nuxt config return

--- woonuxt_base/nuxt.config.ts
+++ woonuxt_base/nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
   'graphql-client': {
     clients: {
       default: {
-        host: process.env.GQL_HOST || 'https://satchart.com/graphql',
+        host: process.env.GQL_HOST || 'http://satchart.com/graphql',
         // corsOptions: { mode: 'cors', credentials: 'include' },
       },
     },
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
   image: {
     provider: "netlify",
     domains: ["vincor.com"],
-  },
+  }, // remove on local ONLY
 
 
 
