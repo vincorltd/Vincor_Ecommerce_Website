a04ca6b004aba2a88514804a99563416d9e2d953
ry-jp <marketing@vincor.com>
2024-10-17 13:01:42 -0500

    accepts multiple images format types

--- woonuxt_base/app/components/BrandImage.vue
+++ woonuxt_base/app/components/BrandImage.vue
@@ -1,27 +1,44 @@
 <script setup lang="ts">
+
 const props = defineProps<{
   product: Product
 }>()
 
 const brandTag = computed(() => {
   const tag = props.product.productTags?.nodes.find(tag => /^[a-zA-Z]+$/.test(tag.name))?.name || '';
-  return tag;
+  return tag.toLowerCase();
 })
 
-const brandImageUrl = computed(() => {
-  return `/images/brands/${brandTag.value.toLowerCase()}.png`
-})
+const extensions = ['png', 'jpg', 'jpeg', 'svg', 'gif']
+const brandImageUrl = ref('')
+
+const checkImageExistence = async () => {
+  for (const ext of extensions) {
+    const url = `/images/brands/${brandTag.value}.${ext}`
+    try {
+      const response = await fetch(url, { method: 'HEAD' })
+      if (response.ok) {
+        brandImageUrl.value = url
+        return
+      }
+    } catch (error) {
+      console.error(`Error checking image: ${url}`, error)
+    }
+  }
+  brandImageUrl.value = '' // No image found
+}
+
+// Call the function when the component is mounted
+onMounted(checkImageExistence)
 </script>
 
 <template>
   <div v-if="brandTag" class="brand-image-container">
-    <span class="flex flex-col gap-2 text-gray-400" v-if="brandImageUrl">
-      Manufacture: <img :src="brandImageUrl" :alt="brandTag" class="brand-image" />
+    <span class="flex flex-col gap-2 text-gray-400">
+      Manufacture: 
+      <img v-if="brandImageUrl" :src="brandImageUrl" :alt="brandTag" class="brand-image" />
+      <span v-else>{{ brandTag }}</span>
     </span>
-    <span v-else>
-      Manufacture: {{ brandTag }}
-    </span>
-    
   </div>
 </template>
 
